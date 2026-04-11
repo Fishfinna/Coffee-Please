af17713d40837d92a0acb005db05539ce627c3c5
Shanti <fishfinna12@gmail.com>
2026-04-10 19:21:33 -0700

    updated timestamps

---
+++ scripts/game/date_time.gd
@@ -0,0 +1,11 @@
+class_name DateTime extends Resource
+
+@export_range(0,59) var seconds: int = 0
+@export_range(0,59) var minutes: int = 0
+@export_range(0,59) var hours: int = 0
+@export var days: int = 0
+
+
+func increase_by_sec(delta_seconds: float) -> void:
+#	contiue https://www.youtube.com/watch?v=PbWtdMQO5jc
+	pass 
