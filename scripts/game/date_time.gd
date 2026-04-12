598c0563463f9fc423fc8ae60276302410f95150
Shanti <fishfinna12@gmail.com>
2026-04-11 19:20:05 -0700

    updated for time logic

--- scripts/game/date_time.gd
+++ scripts/game/date_time.gd
@@ -5,7 +5,20 @@ class_name DateTime extends Resource
 @export_range(0,59) var hours: int = 0
 @export var days: int = 0
 
+var delta_time: float = 0
 
 func increase_by_sec(delta_seconds: float) -> void:
-#	contiue https://www.youtube.com/watch?v=PbWtdMQO5jc
-	pass 
+	delta_time += delta_seconds
+	if delta_time < 1: return
+	
+	var delta_int_secs: int = delta_time 
+	delta_time -= delta_int_secs
+	
+	seconds += delta_int_secs
+	minutes += seconds / 60
+	hours += minutes / 60
+	days += hours / 24
+	
+	seconds = seconds % 60
+	minutes = minutes % 60
+	hours = hours % 24
