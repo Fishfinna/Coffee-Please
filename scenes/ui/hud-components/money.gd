0128742ad8c60e67a5ef196b410cea354cc35cd5
Shanti <fishfinna12@gmail.com>
2026-04-28 20:30:09 -0700

    updated the money system

---
+++ scenes/ui/hud-components/money.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+@onready var money_label: Label = $money/total
+@onready var money_today_label: Label = $money/today
+
+
+var _last_money: int = -1
+var _day_start_money: int = 0
+var _last_today: int = -1
+
+func _ready() -> void:
+	_day_start_money = Global.money
+	_update_labels()
+
+func _process(_delta: float) -> void:
+	if Global.money != _last_money:
+		_update_labels()
+
+func _update_labels() -> void:
+	_last_money = Global.money
+	money_label.text = "Money: %d$" % _last_money
+
+	var today: int = _last_money - _day_start_money
+	if today != _last_today:
+		_last_today = today
+		money_today_label.text = "Today: %d$" % today
