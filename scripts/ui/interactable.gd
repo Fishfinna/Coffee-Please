0e57bc97fef2eeda6ee3023819ffccb29b69d013
Shanti <fishfinna12@gmail.com>
2026-05-03 13:22:34 -0700

    fey animation

--- scripts/ui/interactable.gd
+++ scripts/ui/interactable.gd
@@ -13,13 +13,11 @@ signal customer_exited
 var interact: Callable = func():
 	print("I am a sink!")
 
-
 func _ready() -> void:
 	monitoring = true
 	monitorable = true
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
-
 
 func _on_body_entered(body: Node) -> void:
 	if body.is_in_group("player"):
