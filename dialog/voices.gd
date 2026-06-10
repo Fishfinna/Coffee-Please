ef45b040c7c303a5f3aab723fee9a0a8af04e131
Shanti <fishfinna12@gmail.com>
2026-06-09 19:34:17 -0700

    updated the dialog

--- dialog/voices.gd
+++ dialog/voices.gd
@@ -1,5 +1,5 @@
 const VOICES = {
 	"Fey":  { "sound": "res://assets/audio/voices/fey-voice.wav",  "pitch_min": 0.9, "pitch_max": 1.1 },
-	"Robin":    { "sound": "res://assets/audio/voices/robin-voice.wav",    "pitch_min": 0.9, "pitch_max": 1.1 },
+	"Robin":    { "sound": "res://assets/audio/voices/robin-voice.wav",    "pitch_min": 0.7, "pitch_max": 1 },
 	"Text": { "sound": "res://assets/audio/voices/typewriter-click.ogg", "pitch_min": 0.8, "pitch_max": 1.2 }
 }
