440c6c5b231846271c5382eaf8055b5ce099cab0
Shanti <fishfinna12@gmail.com>
2026-06-19 19:51:18 -0700

    fixed some interaction issues

--- scripts/ui/interacting_component.gd
+++ scripts/ui/interacting_component.gd
@@ -1,35 +1,42 @@
 extends Node2D
-
 @onready var interact_label: Label = $InteractLabel
-
-var current_interactions := []
+var current_interactions: Array[Interactable] = []
 var can_interact := true
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and can_interact:
-		if current_interactions:
+		var target := _get_nearest_interactable()
+		if target:
 			can_interact = false
 			interact_label.hide()
 			
-			await current_interactions[0].interact.call()
+			await target.interact.call()
 			can_interact = true
 
 func _process(_delta: float) -> void:
 	if current_interactions and can_interact:
 		current_interactions.sort_custom(_sort_by_nearest)
-		if current_interactions[0].is_interactable:
-			interact_label.text = current_interactions[0].interact_name
+		var target := _get_nearest_interactable()
+		if target:
+			interact_label.text = target.interact_name
 			interact_label.show()
-	else:
-		interact_label.hide()
+			return
+	interact_label.hide()
 
-func _sort_by_nearest(area1, area2):
-	var area1_dist = global_position.distance_to(area1.global_position)
-	var area2_dist = global_position.distance_to(area2.global_position)
+func _get_nearest_interactable() -> Interactable:
+	for area in current_interactions:
+		if area.is_interactable:
+			return area
+	return null
+
+func _sort_by_nearest(area1: Interactable, area2: Interactable) -> bool:
+	var area1_dist = global_position.distance_squared_to(area1.global_position)
+	var area2_dist = global_position.distance_squared_to(area2.global_position)
 	return area1_dist < area2_dist
 
 func _on_interact_range_area_entered(area: Area2D) -> void:
-	current_interactions.push_back(area)
+	if area is Interactable:
+		current_interactions.push_back(area)
 
 func _on_interact_range_area_exited(area: Area2D) -> void:
 	current_interactions.erase(area)
